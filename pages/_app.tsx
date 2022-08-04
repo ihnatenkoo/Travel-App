4207d598d5ec271da85659059c8d4d6953c18663
Dmytro <ihnatenko.dev@gmail.com>
2022-08-04 20:53:11 +0300

    Install progressbar and sharp. Add progressbar in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
+
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress color="#eb601e" />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
