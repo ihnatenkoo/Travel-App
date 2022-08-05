42e9e6b40829bbb34462419033c682ca41b92ff7
Dmytro <ihnatenko.dev@gmail.com>
2022-08-05 16:39:34 +0300

    Remove zoom from SimpleMap

--- app/components/maps/SimpleMap/SimpleMap.tsx
+++ app/components/maps/SimpleMap/SimpleMap.tsx
@@ -7,32 +7,33 @@ interface ISimpleMap {
 
 export const SimpleMap: FC<ISimpleMap> = ({ country }) => {
   return (
-    <div style={{ padding: '0 0' }}>
-      <ComposableMap>
-        <ZoomableGroup center={[10, 0]} zoom={1.1}>
-          <Geographies geography="/geography.json">
-            {({ geographies }) =>
-              geographies.map((geo) => {
-                const isCurrent = geo.properties.name === country;
+    <ComposableMap
+      projectionConfig={{
+        center: [10, 0],
+        scale: 200,
+      }}
+    >
+      <Geographies geography="/geography.json">
+        {({ geographies }) =>
+          geographies.map((geo) => {
+            const isCurrent = geo.properties.name === country;
 
-                return (
-                  <Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                    fill={isCurrent ? '#eb601e' : '#39373b'}
-                    stroke={isCurrent ? 'transparent' : '#212022'}
-                    style={{
-                      default: { outline: 'none' },
-                      hover: { outline: 'none' },
-                      pressed: { outline: 'none' },
-                    }}
-                  />
-                );
-              })
-            }
-          </Geographies>
-        </ZoomableGroup>
-      </ComposableMap>
-    </div>
+            return (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill={isCurrent ? '#eb601e' : '#39373b'}
+                stroke={isCurrent ? 'transparent' : '#212022'}
+                style={{
+                  default: { outline: 'none' },
+                  hover: { outline: 'none' },
+                  pressed: { outline: 'none' },
+                }}
+              />
+            );
+          })
+        }
+      </Geographies>
+    </ComposableMap>
   );
 };
